3042b74b1a4bd30b9ae6786edad028c6b52ea9d5
munozcar20 <carlosmz243@gmail.com>
2022-07-25 22:18:27 -0500

    Home page list all products registers in cards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,8 @@ import { ProductosComponent } from './productos/productos.component';
 const routes: Routes = [
   {
     path:'',
-    component:ComprarComponent,
+    redirectTo: 'home',
+    pathMatch: 'full'
   },
 
   {
--- src/app/comprar/comprar.component.ts
+++ src/app/comprar/comprar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductosService } from '../services/productos.service';
+import { Productos } from '../shared/model/Productos';
 
 @Component({
   selector: 'app-comprar',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./comprar.component.css']
 })
 export class ComprarComponent implements OnInit {
+  listCharacters:Productos[]=[]
 
-  constructor() { }
 
+  constructor(private svcProductos:ProductosService) { }
   ngOnInit(): void {
+    this.cargarData()
   }
+
+  cargarData(){
+    this.svcProductos.getProductos().subscribe(value=>{
+      const dataObject = Object.values(value);
+      if (dataObject[0] == 200){
+        this.listCharacters = dataObject[2];
+        console.log( this.listCharacters);
+      }
+    });
+  }
+
+
 
 }
